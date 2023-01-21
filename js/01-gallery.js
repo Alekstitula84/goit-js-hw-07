60da7835ad46f978e3421816fda685449766849c
Alekstitula84 <alekstitula84@gmail.com>
2023-01-21 16:31:30 +0200

    done homework7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+
+for (let el of galleryItems) {
+    document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', ` <a class="gallery__link href="${el.original}"><img class="gallery__image" src="${el.preview}" data-source="${el.original}" alt="${el.description}" ></a>`)
+};
+
+const galleryBox = document.querySelector(".gallery");
+
+galleryBox.addEventListener("click", imageClick);
+function imageClick(event) {
+    event.preventDefault();
+    // Перевірка чи клік містить картинку
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    // Відкриття    
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+    instance.show();
+    // Закриття
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+for (let el of galleryItems) {
+    document.querySelector('.gallery').insertAdjacentHTML('beforeEnd', `<a class="gallery__item" href="${el.original}"><img class="gallery__image" src="${el.preview}"  alt="${el.description}"></a>`)
+};
+
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250
+});
+
